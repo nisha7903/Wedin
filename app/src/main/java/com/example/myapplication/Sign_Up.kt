e95734f23ec16528753a805c6202444be99719f0
Anshu <anshukumarsingh448@gmail.com>
2022-02-16 11:37:55 +0530

    Sign In

--- app/src/main/java/com/example/myapplication/Sign_Up.kt
+++ app/src/main/java/com/example/myapplication/Sign_Up.kt
@@ -1,11 +1,20 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Sign_Up : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        supportActionBar?.hide()
+        var login = findViewById<Button>(R.id.sign_in)
+        login.setOnClickListener {
+            val intent = Intent(this@Sign_Up, SignIn::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
